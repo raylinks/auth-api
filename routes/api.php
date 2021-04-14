8a0ee94d6667ce0097fe2981d3d7ff745324a0b5
Raymond Rilwan <olawuyirilwan98@gmail.com>
2021-04-14 16:02:04 +0100

    route for registration

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,10 @@ Route::group(['middleware' => 'auth:sanctum' ], function(){
 
 });
 
+Route::post('register/doctor', [AuthenticationController::class, 'doctorRegister'])->name('register');
+
+Route::post('register/healthseeker', [AuthenticationController::class, 'healthSeekerRegister'])->name('register');
+
 Route::prefix('auth')->name('auth.')->group(function () {
     Route::post('register', [AuthenticationController::class, 'register'])->name('register');
     Route::post('login', [AuthenticationController::class, 'login'])->name('login');
